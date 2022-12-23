3981759fa6cd5bdf4dd48cbe0047e3e6e0eaff3f
Eng:Saeed-Ramadan <83456924+Saeed-Ramadan@users.noreply.github.com>
2022-12-23 18:04:09 +0200

    v6.0 ==> add logo and add style to the navbar and make navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import {ReactComponent as ClownLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <ClownLogo className="logo"/>
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
